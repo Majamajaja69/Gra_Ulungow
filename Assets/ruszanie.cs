0f0ca947067e361e5e9606058f0c3f7bc871e430
Julia <dzula064@gmail.com>
2023-04-01 07:38:14 +0200

    AAAAA

--- Assets/ruszanie.cs
+++ Assets/ruszanie.cs
@@ -81,7 +81,7 @@ public class ruszanie : MonoBehaviour
     }    
 
     private void Jump(){
-        if(_jumpCount < 0.1f){
+        if(_jumpCount < 2){
             rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
         _jumpCount++;
         }        
