b32691eca80b58f135dd1f0cca4093d71e093d6a
Julia <dzula064@gmail.com>
2023-04-01 11:38:35 +0200

    fix

--- Assets/Scenes/Koniec2.cs
+++ Assets/Scenes/Koniec2.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class koniec : MonoBehaviour
+public class Koniec2 : MonoBehaviour
 {
-
+    
 private void OnTriggerEnter2D(Collider2D collision)
 {
-    if(collision.gameObject.tag == "Player"){
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+if(collision.gameObject.tag == "Player"){
+    SceneManager.LoadScene("MainMenu");
     }
-    
 }
+    
 }
